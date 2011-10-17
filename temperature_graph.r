c9f03c3a4c80a61317faf3ea1be582d46af04b3e
Eric Hodel <drbrain@segment7.net>
2011-10-17 11:04:14 -0700

    Add desired temperature to the graph

--- temperature_graph.r
+++ temperature_graph.r
@@ -29,6 +29,13 @@ FROM outside_temperatures
 WHERE ts > now() - interval '24 hours'
 ORDER BY time")
 
+desired <- query(con, "
+SELECT date_trunc('minute', ts) as time, max(temperature) as temp
+FROM desired_temperatures 
+WHERE ts > now() - interval '24 hours'
+GROUP BY date_trunc('minute', ts)
+ORDER BY time")
+
 fire_on <- query(con, "
 SELECT ts as time
 FROM device_history_multi_io
@@ -47,10 +54,9 @@ WHERE dev_name = 'Fireplace'
 x_range   <- as.POSIXct(range(living_room$time, outside$time),
                         "hours", tz="PST")
 y_range   <- range(living_room$temp, outside$temp)
-y_max     <- max(y_range)
-colors    <- c("black", "red")
-plot_char <- c("", "o")
-line_type <- 1:1
+colors    <- c("black", "darkgreen", "red")
+plot_char <- c("", "o", "o")
+line_type <- c(1, 0, 1)
 
 png(filename="/Users/drbrain/Sites/temperature.png",
     height=750, width=1000, bg="white")
@@ -60,8 +66,12 @@ plot(living_room$time, smooth(living_room$temp),
      ylim=y_range, pch=plot_char[1], col=colors[1], type="l",
      ann=FALSE, axes=FALSE)
 
+# desired
+points(desired$time, desired$temp, pch=plot_char[2], col=colors[2])
+text(desired$time, desired$temp, desired$temp, pos=4, col=colors[2])
+
 # outside
-lines(outside$time, outside$temp, type="o", col=colors[2])
+lines(outside$time, outside$temp, type=plot_char[3], col=colors[3])
 
 # fire on vertical lines
 lapply(fire_on$time, function(x) abline(v=x, col="red"))
@@ -85,7 +95,7 @@ title(ylab="Degrees F")
 
 # legend
 legend(min(living_room$time), y_range[2],
-       c("Living Room", "Outside"), col=colors,
+       c("Living Room", "Desired", "Outside"), col=colors,
        pch=plot_char, lty=line_type)
 
 dev.off()
